bc32f5c0427ba308f83c9b4fd4162b264eecfc53
muh-avid <avidm8434@gmail.com>
2023-12-27 23:29:39 +0800

    addMenuToggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-  const [active, setActive] = useState('`')
+  const [active, setActive] = useState('`');
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav
@@ -39,6 +40,15 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className="sm:hiddem flex flex-1 justify-end items-center">
+          <img 
+          src={toggle ? close : menu} 
+          alt="menu" 
+          className="w-[28px] h [28px] object-contain cursor-pointer" 
+          onClick={() => setToggle(!toggle)}
+          />
+        </div>
       </div>
     </nav>
   )
