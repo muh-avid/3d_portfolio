e58239110ac42733720e6ab7740cf8260aa23f15
muh-avid <avidm8434@gmail.com>
2024-01-07 23:33:22 +0800

    editHeroFile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  // https://youtu.be/0fYi8SGA20k?t=2985
+  // const computer = useGLTF('.desktop_pc/scene.gltf')
+
   return (
     <div>Computers</div>
   )
