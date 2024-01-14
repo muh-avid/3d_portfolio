c09a7121ad0f69e3372a0469f7e706e23f33a011
muh-avid <avidm8434@gmail.com>
2024-01-14 22:55:16 +0800

    editCanvasLoader

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ### masalah yang terjadi selama mengerjakan project
 ✔ * 26 - dec - 2023 | 06 - januari - 2024 : belum bisa solved bagian toggle dropdown https://youtu.be/0fYi8SGA20k?t=2328 ini bagian  toggle yang belum bisa dipecahkan 
 
-__coba dilanjutkan besok yaaah harus semangat
+__coba dilanjutkan besok yaaah harus semangat https://youtu.be/0fYi8SGA20k?t=3582 bagian ini
 
 #### 
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,12 +9,20 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.40} groundColor="black"/>
-      <pointLight intensity={5} />
+      <hemisphereLight intensity={1.50} groundColor="black"/>
+      <pointLight intensity={7} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
-        scale={0.65}
-        position={[0, -3.25, -1.5]}
+        scale={0.75}
+        position={[0, -5.44, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
